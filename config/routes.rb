6f19d41549d53aaed4ea0463300a333d861037da
Maulana Adil <maulanaadil.latif@gmail.com>
2022-04-28 11:13:19 +0700

    adding logout session

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/sign_up", to: "registrations#new"
   post "/sign_up", to: "registrations#create"
+  delete "/logout", to: "sessions#destroy"
 
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
