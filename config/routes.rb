076437dfa816a68fb0cd076bb4a0dcdd24e08ee6
Maulana Adil <maulanaadil.latif@gmail.com>
2022-04-29 16:24:13 +0700

    setup to twitter API credentials and adding auth on twitter API also adding twitter account model

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   delete "/logout", to: "sessions#destroy"
 
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
