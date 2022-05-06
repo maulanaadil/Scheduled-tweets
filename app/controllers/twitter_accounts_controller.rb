145210f5c19e2ba5c3527447805acd2cb2a712ad
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-06 15:07:49 +0700

    add twitter accounts page & adding feature disconnect from twitter also adding condition if user doesnt login then redirect to sign in page

---
+++ app/controllers/twitter_accounts_controller.rb
@@ -0,0 +1,14 @@
+class TwitterAccountsController < ApplicationController
+
+    before_action :require_user_logged_in!
+
+    def index
+        @twitter_accounts = Current.user.twitter_accounts
+    end
+
+    def destroy
+        @twitter_account = Current.user.twitter_accounts.find(params[:id])
+        @twitter_account.destroy
+        redirect_to twitter_accounts_path, notice: "Successfully disconnected user_id #{@twitter_account.user_id}"
+    end    
+end    
